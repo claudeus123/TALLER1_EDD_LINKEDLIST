fe991df5d853c56dc7a1acd5e22252889eff39f3
claudeus123 <112731006+claudeus123@users.noreply.github.com>
2022-10-07 22:16:19 -0300

    Update linkedList.h

--- linkedList.h
+++ linkedList.h
@@ -6,13 +6,19 @@
 class LinkedList{
   Node* first;
   public:
-    Node* getFirst();
-    LinkedList();
-    ~LinkedList();
+    Node* getFirst(); //Get del primer elemento de la lista
+    LinkedList(); // COnstructor
+    ~LinkedList(); //Destructor 
+    
+    //Agrega un nodo como primero de la lista
     void add(Region* region);
+    //Suma 1 a la region correspondiente
     void sum(Node* node);
+    //Ordena la lista de menor a mayor
     void sort();
+    //swap no implementado
     Node* swap(Node* ptr1, Node* ptr2);
+    //Comprueba si existe un nodo
     Node* exists(std::string code);
 };
 #endif
